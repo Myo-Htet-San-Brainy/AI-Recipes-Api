859602b842bd8dd3cb1a94051a331d16a17c7a5e
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2024-04-12 02:43:44 +0700

    added imagetoText func

--- utils/ai.cjs
+++ utils/ai.cjs
@@ -1,11 +1,12 @@
 require("dotenv").config();
+const fs = require("fs");
 const history = require("./history.cjs");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-async function run(prompt) {
+async function textRun(prompt) {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
   const chat = model.startChat({
@@ -16,10 +17,36 @@ async function run(prompt) {
   });
   const result = await chat.sendMessage(prompt);
   const response = await result.response;
-  console.log("response:", response);
+  console.log("textRun response:", response);
+  return response.text();
+}
+
+// Converts local file information to a GoogleGenerativeAI.Part object.
+function fileToGenerativePart(path, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType,
+    },
+  };
+}
+
+async function imageRun() {
+  // For text-and-image input (multimodal), use the gemini-pro-vision model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+  const prompt = "Give me ingredients from the provided image.";
+
+  const result = await model.generateContent([
+    prompt,
+    fileToGenerativePart("image1.png", "image/png"),
+  ]);
+  const response = await result.response;
+  console.log("imageRun response:", response);
   return response.text();
 }
 
 module.exports = {
-  run,
+  textRun,
+  imageRun,
 };
--- utils/aiHelper.js
+++ utils/aiHelper.js
@@ -1,7 +1,7 @@
-import { run } from "./ai.cjs";
+import { textRun } from "./ai.cjs";
 
 export async function generateRecipesHelper(ingredients, restrictions) {
   const formattedPrompt = `These are ingredients - ${ingredients}. These are dietary restrictions - ${restrictions}. Is there any recipe you can provide?`;
-  const output = await run(formattedPrompt);
+  const output = await textRun(formattedPrompt);
   return output;
 }
